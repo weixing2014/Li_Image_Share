ee9b3e8143bebced553b4df97a3e3feecfbbc7e4
Li Zhang <ycy828f@gmail.com>
2016-01-26 15:36:56 -0800

    Add the ability to create a new image

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,4 +2,19 @@ class ImagesController < ApplicationController
   def new
     @image = Image.new
   end
+
+  def create
+    url    = params[:image][:url]
+    @image = Image.new(url: url)
+    if @image.save
+      redirect_to image_path(@image)
+    else
+      flash.now[:error] = @image.errors.full_messages.first
+      render :new
+    end
+  end
+
+  def show
+    @image = Image.find(params[:id])
+  end
 end
