2a6f226a0045946878619d3dd333430ec5cc96bb
Li Zhang <ycy828f@gmail.com>
2016-01-28 14:37:26 -0800

    Fix formatting and use assert_difference

--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -2,39 +2,51 @@ require 'test_helper'
 class ImagesControllerTest < ActionController::TestCase
   VALID_IMAGE_URL = 'https://upload.wikimedia.org/wikipedia/en/thumb/e/e9/Ruby_on_Rails.svg/791px-Ruby_on_Rails.svg.png'
 
-  test "should get index" do
+  test 'should get index' do
     get :index
     assert_response :success
   end
 
-  test "create a new image" do
-    post :create, image: {url: VALID_IMAGE_URL}
+  test 'create a new image' do
+    assert_difference('Image.count', 1) do
+      post :create, image: { url: VALID_IMAGE_URL }
+    end
+
     assert_redirected_to image_path(assigns(:image))
   end
 
-  test "fail to create a new image" do
-    post :create, image: {url: ""}
+  test 'fail to create a new image' do
+    assert_no_difference('Image.count') do
+      post :create, image: { url: '' }
+    end
+
     assert_response :unprocessable_entity
     assert_equal "Url can't be blank", flash[:error]
     assert_select 'form#new_image'
   end
 
-  test "show new image page" do
+  test 'show new image page' do
     get :new
     assert_response :success
     assert_select 'form#new_image'
   end
 
-  test "show an image" do
+  test 'show an image' do
     image = Image.create(url: VALID_IMAGE_URL)
     get :show, id: image.id
     assert_response :success
     assert_select 'img' do |elements|
-      assert_equal [image.url], elements.map {|el| el[:src]}
+      assert_equal [image.url], elements.map { |el| el[:src] }
     end
   end
 
-  test "display all saved images on index page" do
+  test 'fail to show an image' do
+    assert_raises ActiveRecord::RecordNotFound do
+      get :show, id: -1
+    end
+  end
+
+  test 'display all saved images on index page' do
     7.times do
       Image.create!(url: VALID_IMAGE_URL)
     end
@@ -42,7 +54,8 @@ class ImagesControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_select 'img.collection_image' do |elements|
-      assert_equal [VALID_IMAGE_URL] * Image.count, elements.map { |el| el[:src] }
+      assert_equal [VALID_IMAGE_URL] * Image.count,
+                   elements.map { |el| el[:src] }
     end
   end
 end
