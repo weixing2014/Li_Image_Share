785a3ce8d485eb23cbfe7ee8774503b8b6c31905
Li Zhang <ycy828f@gmail.com>
2016-01-29 14:15:20 -0800

    Change UrlValidator to validate HTTP and HTTPs protocols only

--- app/validators/url_validator.rb
+++ app/validators/url_validator.rb
@@ -1,7 +1,7 @@
 require 'uri'
 class UrlValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    unless value.blank? || value =~ /\A#{URI.regexp}\z/
+    unless value.blank? || value =~ /\A#{URI.regexp(%w(http https))}\z/
       record.errors[attribute] << (options[:messages] || 'is invalid!')
     end
   end
