4d6301ea83258dee244d688d6fee1fd95d95e404
Li Zhang <ycy828f@gmail.com>
2016-02-01 13:36:28 -0800

    Add 20 images for seeding
    Add 20 different .gif images for seeding
    
    Use %w instead of array literal in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+urls = %w(
+  https://i.chzbgr.com/full/8477892864/h50B3AFC3/
+  https://media.giphy.com/media/3o7ZeEqF00F2pI0moU/giphy.gif
+  https://media.giphy.com/media/Mnw7GYKVJjX20/giphy.gif
+  https://media.giphy.com/media/ldD9copTuuNNe/giphy.gif
+  https://media.giphy.com/media/lJav8npz16Ocg/giphy.gif
+  https://media.giphy.com/media/1395cV7aUaBhlK/giphy.gif
+  https://media.giphy.com/media/TF5jiKi9uGibu/giphy.gif
+  https://media.giphy.com/media/l39JrKv9ZZX2g/giphy.gif
+  https://media.giphy.com/media/xIJLgO6rizUJi/giphy.gif
+  https://media.giphy.com/media/26hlRUmCJ0qWQHeaQ/giphy.gif
+  https://media.giphy.com/media/BB67sSfKNEnfO/giphy.gif
+  https://media.giphy.com/media/DhnSIHPkuXgFa/giphy.gif
+  https://media.giphy.com/media/sn9HA6bwiyB2w/giphy.gif
+  https://media.giphy.com/media/26tnndHUXftq2IFpe/giphy.gif
+  https://media.giphy.com/media/Ay8BzrcBh85pK/giphy.gif
+  https://media.giphy.com/media/e07y5SEwFMDm0/giphy.gif
+  https://media.giphy.com/media/MF4XbEUxTXKfK/giphy.gif
+  https://media.giphy.com/media/PHkIfAw1FUUms/giphy.gif
+  https://media.giphy.com/media/Yy7BiuPbeaGXu/giphy.gif
+  https://media.giphy.com/media/13uaMxgBhGP9ba/giphy.gif
+)
+
+urls.each { |url| Image.create!(url: url) }
