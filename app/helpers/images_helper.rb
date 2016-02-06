9133a10176236ad73c3e2723818d528f73072652
Li Zhang <ycy828f@gmail.com>
2016-02-05 16:54:34 -0800

    lz - Refactor images helper methods into partial structure

--- app/helpers/images_helper.rb
+++
@@ -1,9 +0,0 @@
-module ImagesHelper
-  def render_images(images)
-    render partial: 'shared/image', collection: images
-  end
-
-  def render_tag_links(links)
-    render partial: 'shared/tag_link', collection: links
-  end
-end
