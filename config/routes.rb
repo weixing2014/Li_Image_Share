a3f471f2af3f2a003af187a075731ab162cc5883
Li Zhang <ycy828f@gmail.com>
2016-02-08 15:06:00 -0800

    lz - Restrict images routes to exclude unused update and edit path

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'images#index'
 
-  resources :images
+  resources :images, except: [:update, :edit]
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
