874122eb47a86e4fd69ff97a576b2f055de1dfeb
Li Zhang <ycy828f@gmail.com>
2016-02-09 13:53:03 -0800

    Add ES6 module into project

--- Gemfile
+++ Gemfile
@@ -26,6 +26,8 @@ gem 'puma'
 
 gem 'acts-as-taggable-on', '~> 3.4'
 
+gem 'browserify-rails'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module LiImageShare
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.browserify_rails.commandline_options = "-t babelify"
     ActsAsTaggableOn.remove_unused_tags = true
   end
 end
