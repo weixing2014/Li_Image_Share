91f4263bcc39b9eb5243f04f1f87cb5c8d3638a8
Li Zhang <ycy828f@gmail.com>
2016-02-11 08:41:43 -0800

    lz - Style pages with bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def render_nav_item(content, active)
+    classes = 'nav-item'
+    classes << ' active' if active
+    content_tag :li, content, class: classes
+  end
 end
