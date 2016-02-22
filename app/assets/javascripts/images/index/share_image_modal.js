c5209914078e5c246c472ac20280c9171b6a58c1
Li Zhang <ycy828f@gmail.com>
2016-02-22 13:48:17 -0800

    lz - Explicitly exclude share image send button from being value emptied

--- app/assets/javascripts/images/index/share_image_modal.js
+++ app/assets/javascripts/images/index/share_image_modal.js
@@ -13,7 +13,7 @@ class ShareImageModal  {
   }
 
   resetToBlankForm() {
-    this.$modal.find('input').not(':last').val('');
+    this.$modal.find('input').not('input[type=submit]').val('');
     this.$modal.find('.js-error-message').remove();
   }
 
