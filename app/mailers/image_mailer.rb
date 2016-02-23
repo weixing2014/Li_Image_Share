4da3b3b7541f17695a221266491837bd07212547
Li Zhang <ycy828f@gmail.com>
2016-02-23 08:22:57 -0800

    lz - Use if instead of unless in ImageMailer

--- app/mailers/image_mailer.rb
+++ app/mailers/image_mailer.rb
@@ -2,7 +2,7 @@ class ImageMailer < ApplicationMailer
   def share_image_email(image, recipient, subject)
     @image = image
 
-    subject = 'An image shared from FunGraph' unless subject.present?
+    subject = 'An image shared from FunGraph' if subject.blank?
 
     mail(to: recipient, subject: subject)
   end
