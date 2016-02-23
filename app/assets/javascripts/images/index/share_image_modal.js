8801784aa80decda61de9c02ff62b40182928b31
Li Zhang <ycy828f@gmail.com>
2016-02-23 11:45:49 -0800

    lz - use input:not([type=submit]) css selector instead of an extra not jquery filter to keep submit button value from removal upon share image modal reopen so that the code is more efficient and clear

--- app/assets/javascripts/images/index/share_image_modal.js
+++ app/assets/javascripts/images/index/share_image_modal.js
@@ -13,7 +13,7 @@ class ShareImageModal  {
   }
 
   resetToBlankForm() {
-    this.$modal.find('input').not('[type=submit]').val('');
+    this.$modal.find('input:not([type=submit])').val('');
     this.$modal.find('.js-error-message').remove();
   }
 
