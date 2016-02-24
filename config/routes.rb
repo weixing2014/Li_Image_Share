8bcd25fe3bd6ce329348123663fc4f2468f724ed
Li Zhang <ycy828f@gmail.com>
2016-02-24 14:21:21 -0800

    lz - Add UsersController actions, routes and tests for sign up

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
 
   resources :tags, only: [:index]
 
+  get 'signup' => 'users#new'
+  resources :users, only: [:create]
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,35 @@
+require 'test_helper'
+class UsersControllerTest < ActionController::TestCase
+  test 'fail to sign up with invalid information' do
+    assert_no_difference 'User.count' do
+      post :create, user: { name: ' ',
+                            email: 'user@invalid',
+                            password: 'foo',
+                            password_confirmation: 'bar' }
+    end
+
+    assert_response :unprocessable_entity
+    assert_select 'span.help-block' do |elements|
+      assert_equal ["can't be blank",
+                    'is invalid',
+                    'is too short (minimum is 6 characters)',
+                    "doesn't match Password"],
+                   elements.map(&:text)
+    end
+    assert_select 'form#new_user'
+  end
+
+  test 'create a new user successfully' do
+    assert_difference 'User.count', 1 do
+      post :create, user: { name: 'Li Zhang',
+                            email: 'user@example.com',
+                            password: 'foobar',
+                            password_confirmation: 'foobar' }
+    end
+
+    assert_equal 'Li Zhang', User.last.name
+    assert_equal 'user@example.com', User.last.email
+    assert_equal 'You have successfully signed up!', flash[:notice]
+    assert_redirected_to root_path
+  end
+end
