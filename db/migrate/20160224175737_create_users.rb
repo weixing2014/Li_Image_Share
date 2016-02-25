3160af2873d766b2dd339b229322c30c75f6b251
Li Zhang <ycy828f@gmail.com>
2016-02-25 09:39:36 -0800

    lz - Combine all the User model migrations into one

--- db/migrate/20160224175737_create_users.rb
+++ db/migrate/20160224175737_create_users.rb
@@ -4,8 +4,11 @@ class CreateUsers < ActiveRecord::Migration
       t.string :name, null: false
       t.string :email, null: false
       t.string :password_digest, null: false
+      t.string :remember_digest
 
       t.timestamps null: false
     end
+
+    add_index :users, :email, unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160225032343) do
+ActiveRecord::Schema.define(version: 20160224175737) do
 
   create_table "images", force: :cascade do |t|
     t.string   "url",        null: false
@@ -43,9 +43,9 @@ ActiveRecord::Schema.define(version: 20160225032343) do
     t.string   "name",            null: false
     t.string   "email",           null: false
     t.string   "password_digest", null: false
+    t.string   "remember_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.string   "remember_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
