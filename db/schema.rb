c2965768506d6017ed9fca6273707b7538653e5d
Li Zhang <ycy828f@gmail.com>
2016-02-25 08:46:50 -0800

    lz - Remove add_password_digest_to_users migration, instead add this column in create_users migration and declare it as null false

--- db/schema.rb
+++ db/schema.rb
@@ -42,9 +42,9 @@ ActiveRecord::Schema.define(version: 20160225032343) do
   create_table "users", force: :cascade do |t|
     t.string   "name",            null: false
     t.string   "email",           null: false
+    t.string   "password_digest", null: false
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.string   "password_digest"
     t.string   "remember_digest"
   end
 
