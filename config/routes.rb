7502b6b084525f4f7c4bff445570f0a92e6e1f05
Li Zhang <ycy828f@gmail.com>
2016-02-25 08:26:22 -0800

    lz - Align routes in routes.rb and group related routes together instead of messing them up

--- config/routes.rb
+++ config/routes.rb
@@ -5,18 +5,18 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'images#index'
 
+  get 'signup' => 'users#new'
+  resources :users, only: [:create]
+
+  get    'login'  => 'sessions#new'
+  post   'login'  => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+
   resources :images do
     post :share, on: :member
   end
 
   resources :tags, only: [:index]
-
-  get 'signup' => 'users#new'
-  get 'login' => 'sessions#new'
-  post 'login' => 'sessions#create'
-  delete 'logout' => 'sessions#destroy'
-
-  resources :users, only: [:create]
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
