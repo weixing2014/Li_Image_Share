d7ccdb59b8193f5f7ec73dab3e1e91adc1f2355a
Li Zhang <ycy828f@gmail.com>
2016-02-24 19:11:35 -0800

    lz - Log in user after a successful sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      log_in @user
       flash[:notice] = 'You have successfully signed up!'
       redirect_to root_path
     else
